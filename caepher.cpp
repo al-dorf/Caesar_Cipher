8f3736c2e826f5b8ec89ac7ccfe61666c97a84ab
Aldorf <alwyneuendorf@hotmail.com>
2019-02-19 16:56:31 +1000

    Added Decryption function

--- caepher.cpp
+++ caepher.cpp
@@ -5,8 +5,10 @@ using namespace std;
 
 char encrypt(char pch);
 
-// char Caesar::decrypt(char cch)
-string p{},c{};
+char decrypt(char cch);
+
+string p = "";
+string c = "";
 int key{};
 string alphabet{"abcdefghijklmnopqrstuvwxyz"};
 
@@ -21,12 +23,23 @@ int main(){
 	cout << "Enter a key: ";
 	cin >> key;
 
+	cout << "\n";
+
+	cout << "Plaintext before encryption: " << p << endl;
 	for(unsigned int i=0; i<p.length(); i++){		
 		c += encrypt(p[i]);
 	}
+	p = "";
+	cout << "Plaintext after encryption: " << p << endl;
 
-	cout << "Plaintext: " << p << endl;
-	cout << "Cipher: " << c << endl;
+	cout << "\n";
+	
+	cout << "Ciphertext before decryption: " << c << endl;
+	for(unsigned int i=0; i<c.length(); i++){		
+		p += decrypt(c[i]);
+	}
+	c = "";
+	cout << "Ciphertext after decryption: " << p << endl;
 
 return 0;
 }
@@ -40,12 +53,12 @@ char encrypt(char pch){
     return alphabet[ch];
 }
 
-/* char decrypt(char cch){
+char decrypt(char cch){
    int letter {0}, ch{};
     while(cch != alphabet[letter]){
-        letter++
+        letter++;
     }
-    ch = ((letter+1)-key) % 26;
+    ch = (letter-key) % 26;
     return alphabet[ch];  
 }
-*/
+
