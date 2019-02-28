b25cc6df97c8e51943fef5e969d5b613c10c9e70
Aldorf <alwyneuendorf@hotmail.com>
2019-02-28 22:18:23 +1000

    Added I/O for files

--- caepher.cpp
+++ caepher.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
+#include <cctype>
 
 using namespace std;
 
 char encrypt(char pch);
 
 char decrypt(char cch);
+
+// void add_words();
+
+vector<string> enc_vec;
+vector<string> input; 
 
 string p = "";
 string c = "";
@@ -14,32 +22,49 @@ string alphabet{"abcdefghijklmnopqrstuvwxyz"};
 
 
 int main(){
-
-	cout << "Enter a word(s) to be encrypted: ";
-	cin >> p;
-	cin.clear();
-	cin.ignore(1000, '\n');
+	ifstream infile("plain.dat");
+	ofstream outfile("cipher.dat");
+	string line;
+	vector<string> input; 
 
 	cout << "Enter a key: ";
 	cin >> key;
 
 	cout << "\n";
 
-	cout << "Plaintext before encryption: " << p << endl;
-	for(unsigned int i=0; i<p.length(); i++){		
-		c += encrypt(p[i]);
+	if( infile.good() == false){
+		cout << "Unable to open plaintext file\n";
+		exit(1);
 	}
-	p = "";
-	cout << "Plaintext after encryption: " << p << endl;
 
-	cout << "\n";
-	
-	cout << "Ciphertext before decryption: " << c << endl;
-	for(unsigned int i=0; i<c.length(); i++){		
-		p += decrypt(c[i]);
+	cout << "Reading plaintext file\n";
+
+	while( !infile.eof() ){
+		getline(infile, line);
+		input.push_back(line);
 	}
-	c = "";
-	cout << "Ciphertext after decryption: " << p << endl;
+
+	for(unsigned int j=0; j<input.size(); j++){
+		p = input[j];
+		for(unsigned int i=0; i<p.length(); i++){	
+			c += encrypt(p[i]);
+		}
+			enc_vec.push_back(c);
+			p = "";
+			c = "";
+	}
+
+	cout << "Writing encryted words to file\n";
+
+	for(unsigned int j=0; j<enc_vec.size(); j++){
+		p = enc_vec[j];
+		for(unsigned int i=0; i<c.length(); i++){	
+			p += decrypt(c[i]);
+		}
+			outfile << p << endl;
+			p = "";
+			c = "";
+	}
 
 return 0;
 }
@@ -61,4 +86,10 @@ char decrypt(char cch){
     ch = (letter-key) % 26;
     return alphabet[ch];  
 }
-
+/*
+void add_words(){
+	cout << "Enter a word(s) to be encrypted: ";
+	cin >> p;
+	cin.clear();
+	cin.ignore(1000, '\n');
+} */
